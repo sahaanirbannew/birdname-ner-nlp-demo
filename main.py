81ea9863afcf73f02e5e1ce9381900e615af5923
Anirban Saha <mailme@anirbansaha.com>
2022-09-01 17:16:44 +0530

    main.py
    please commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import spacy
+import re
+from zipfile import ZipFile
+
+
+
+colors = {"BIRDNAME": "#8de8e8"}
+options = {"colors": colors} 
+
+
+def extract_model_best_from_archive():
+  file_name = "model-best.zip"
+  with ZipFile(file_name, 'r') as zip:
+    zip.extractall("/content/model-best")
+
+def basic_preprocess(tweet):
+  import preprocessor as p
+  p.set_options(p.OPT.EMOJI, p.OPT.MENTION, p.OPT.URL, p.OPT.SMILEY, p.OPT.NUMBER,p.OPT.HASHTAG)
+  tweet = tweet.lower()
+  tweet = tweet.replace("\n"," ")  
+  tweet = tweet.replace("\\n"," ")
+  tweet = tweet.replace("'","") 
+  tweet = p.clean(tweet)
+  tweet = re.sub(r'[^\w\s]', ' ', tweet)
+  tweet = re.sub(r' x..', '', tweet)
+  tweet = re.sub(r' +', ' ', tweet) #' +', ' '
+  #tweet = re.sub(r' n. ', '', tweet) 
+  tweet = tweet.replace("x9c","")
+  tweet = tweet.strip()
+  return tweet
+
+extract_model_best_from_archive()
+nlp_ner = spacy.load("model-best") 
+
+
+def app_run(sentence):
+  sentence = basic_preprocess(sentence)
+  doc = nlp_ner(sentence)
+  spacy.displacy.render(doc, style="ent", options= options, jupyter=True)
+
+app_run("some nice asian koel songs here")
