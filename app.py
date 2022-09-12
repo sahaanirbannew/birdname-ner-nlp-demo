b569ea1377300be9f1665a9b0389610e269830ae
Anirban Saha <mailme@anirbansaha.com>
2022-09-12 11:45:22 +0530

    ,,,,
    oooo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 import urllib.request
 import html.parser 
 from requests.exceptions import HTTPError
-#from socket import error as SocketError
+from socket import error as SocketError
 #from http.cookiejar import CookieJar
 from urllib.request import build_opener, HTTPCookieProcessor
 
